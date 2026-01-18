c64e99f983cdb2cbc5ad5778b3dabdcb459ad8ff
kinoubenkyou <kinoubenkyou@gmail.com>
2026-01-18 14:13:43 +0700

    add Session model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_18_041932) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_18_071013) do
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
@@ -18,4 +27,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_18_041932) do
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
+
+  add_foreign_key "sessions", "users"
 end
